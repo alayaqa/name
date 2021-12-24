1bddad13d014fbc2f91e9f754e8e3a81e5a1d03b
alayaqa <93867012+alayaqa@users.noreply.github.com>
2021-12-24 19:57:22 +0400

    Add files via upload

---
+++ new/new.py
@@ -0,0 +1,20 @@
+# Print the square of first 10 numbers ( 1 to 10)
+for i in range(1,10):
+  print(i*i)
+
+  
+# Print the series of first 10 even numbers ( 2 to 20)
+for i in range(2,20,2): 
+     print(i)
+
+# Print the series of first 10 even numbers in reverse ( 2 to 20)
+for i in range(2,20,-2): 
+     print(i)
+
+# Relational and Equality Operators
+a=30
+b=20
+if a==b:
+  print('a is equal to b')
+else:
+  print('a and b are not equal')
